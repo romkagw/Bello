88cedca0ee46addc2ccd9f7d621878cb40fad71f
ROMAN <83337336+romkagw@users.noreply.github.com>
2023-03-17 14:20:25 +0200

    removed "await" from line "await response.data; "

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import instance from './request';
 const fetchData = async () => {
 	try {
 		const response = await instance.get();
-		return await response.data;
+		return response.data;
 	} catch (e) {
 		throw new Error(e);
 	}
